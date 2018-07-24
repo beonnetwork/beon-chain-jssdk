96272a033aa5bba6a7e894cc306cbeb45f56ebc7
Surat Teerapittayanon <steerapi@gmail.com>
2018-07-24 20:42:49 +0700

    Clean up console.log

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Web3 = require("web3");
 
 const BrandedTokenFactoryArtifacts = require("./contracts/BrandedTokenFactory.json");
 const BrandedTokenArtifacts = require("./contracts/BrandedToken.json");
-const RootChainArtifacts = require("./contracts/RootChain.json");
+// const RootChainArtifacts = require("./contracts/RootChain.json");
 const utils = require("./lib/utils");
 const Block = require("./lib/Block");
 const Transaction = require("./lib/Transaction");
@@ -25,7 +25,6 @@ class BeOnSDK {
       gas,
       gasPrice
     };
-    // console.log(this.config);
     if (this.config.host.indexOf("https") >= 0) {
       this.client = jayson.client.https(this.config.host);
     } else {
@@ -151,7 +150,6 @@ class BeOnSDK {
     });
 
     let json = response.result.result;
-    console.log("response", response);
     let tx = Transaction.fromJSON(json);
 
     // let prefix = "\x19Ethereum Signed Message:\n32";
@@ -178,8 +176,7 @@ class BeOnSDK {
       response = await this.client.request("getLatestBlock", {}).catch(err => {
         throw err;
       });
-      console.log("response", response);
-      if(!response || !response.result || !response.result.result){
+      if (!response || !response.result || !response.result.result) {
         throw "Blocks do not exist";
       }
       let block = Block.fromJSON(response.result.result);
@@ -187,12 +184,10 @@ class BeOnSDK {
       root = utils.addHexPrefix(block.blockHeader.merkleRoot);
       let txData = tx.data();
       found = block.transactions.findIndex(_tx => {
-        console.log("compare", _tx, txData);
         return _tx === txData;
       });
       nTries += 1;
     }
-    console.log("found", found, nTries);
     if (found < 0) {
       throw "Transaction not found or has yet been mined";
     }
@@ -205,9 +200,6 @@ class BeOnSDK {
     // );
     let confirmationHashed = this.web3.utils.soliditySha3(tx.hash(), root);
     let confirmSignature = await this.web3.eth.accounts.sign(confirmationHashed, key).signature;
-    console.log("hash", this.web3.utils.soliditySha3(tx.hash(), root));
-    console.log("prefixed hash", confirmationHashed);
-    console.log("confirmSignature", confirmSignature);
 
     // submit confirm
     response = await this.client.request("confirmTx", {
@@ -218,7 +210,6 @@ class BeOnSDK {
       throw err;
     });
 
-    console.log("confirm", response);
     return response;
   }
 
@@ -236,16 +227,11 @@ class BeOnSDK {
       throw "transactions do not exist";
     }
     let data = block.transactions[txIndex];
-    // let rawData = utils.removeHexPrefix(data);
-    // let txData = utils.addHexPrefix(rawData.slice(0, rawData.length - 130 - 130));
-    // let sig1 = utils.addHexPrefix(rawData.slice(rawData.length - 130 - 130, rawData.length - 130));
-    // console.log("data", data)
     let {
       txBytes,
       sig1,
       sig2
     } = Block.splitTxData(data);
-    // console.log("txBytes", txBytes);
     let txHash = Web3.utils.sha3(txBytes);
     let root = utils.addHexPrefix(block.blockHeader.merkleRoot);
     return {
@@ -263,11 +249,7 @@ class BeOnSDK {
    * @param {string} key 
    */
   _confirmSig(txHash, root, key) {
-    console.log("confirmSigHash", txHash, root, Web3.utils.soliditySha3(txHash, root));
-
-    // console.log(web3.eth.accounts.sign, utils.concatHex(txHash, root), key);
     let confirmSignature = this.web3.eth.accounts.sign(Web3.utils.soliditySha3(txHash, root), key).signature;
-    console.log("confirmSignature", confirmSignature);
     return confirmSignature;
   }
 
@@ -290,22 +272,16 @@ class BeOnSDK {
       check1 = (utils.removeHexPrefix(account1.toLowerCase()) == utils.removeHexPrefix(account2.toLowerCase()));
       // var prefix = "\x19Ethereum Signed Message:\n32";
       // return Web3.utils.soliditySha3(prefix, message);
-      console.log("check1", check1, root, confSig1, sig2, account1, account2);
-
       if (sig2 && sig2 != "0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") {
         let account1_2 = this.web3.eth.accounts.recover(confirmationHash, confSig2);
         let account2_2 = this.web3.eth.accounts.recover(txHash, sig2);
         check2 = (utils.removeHexPrefix(account1_2.toLowerCase()) == utils.removeHexPrefix(account2_2.toLowerCase()));
-        console.log("check2", check2);
-
       }
-
       return check1 && check2;
       // let account1ec = utils.ecRecover(confirmationHash, confSig1);
       // let account2ec = utils.ecRecover(txHash, sig1);
       // return utils.removeHexPrefix(account1ec.toLowerCase()) == utils.removeHexPrefix(account2ec.toLowerCase());
     } catch (e) {
-      console.log("error", e)
       return false
     }
   }
@@ -369,10 +345,6 @@ class BeOnSDK {
     });
 
     let data = response.result.result;
-
-    // console.log("json", data.result);
-    // console.log("data.utxoPos", data.utxoPos);
-    // console.log("data.result", data.result);
     return this._signedAndSubmit(data.result, key);
   }
 
